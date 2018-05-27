b2319117839993f4502a7e91bbb0ae1d57a11d28
inabajunmr <inabajun.for.regi@gmail.com>
2018-05-28 01:14:54 +0900

    add

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 module.exports = (robot) => {
-  robot.on('issues.opened', async context => {
-    context.log(context.payload)
-    exist_label = context.payload.issue.labels.length != 0;
-    exist_milestone = context.payload.issue.milestone != '';
-    context.log("EXIST LABEL:" + exist_label);
-    context.log("EXIST MILESTONE:" + exist_milestone);
+  robot.on('issues.closed', async context => {
+    const exist_label = context.payload.issue.labels.length != 0;
+    const exist_milestone = context.payload.issue.milestone != '';
     if (exist_label && exist_milestone) {
       return;
     }
-    const comment = context.issue({body: 'ラベルとマイルストーンをつけてくれ〜'})
+    const comment = context.issue({body: 'You must add milestone and label!'})
     context.github.issues.createComment(comment)
-    const close = context.issue({state: 'closed'})
+    const close = context.issue({state: 'open'})
     context.github.issues.edit(close)
   })
 }
+
 
