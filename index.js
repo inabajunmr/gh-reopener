65d36d0b3d2083ecea0a45ec31ab73b773debf9d
inabajunmr <inabajun.for.regi@gmail.com>
2018-05-28 00:13:25 +0900

    modify

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 module.exports = (robot) => {
-  // Your code here
-  robot.log('Yay, the app was loaded!')
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  robot.on('issues.opened', async context => {
+    context.log(context.payload)
+    exist_label = context.payload.issue.labels.length != 0;
+    exist_milestone = context.payload.issue.milestone != '';
+    context.log("EXIST LABEL:" + exist_label);
+    context.log("EXIST MILESTONE:" + exist_milestone);
+    if (exist_label && exist_milestone) {
+      return;
+    }
+    const comment = context.issue({body: 'ラベルとマイルストーンをつけてくれ〜'})
+    context.github.issues.createComment(comment)
+    const close = context.issue({state: 'closed'})
+    context.github.issues.edit(close)
+  })
 }
+
